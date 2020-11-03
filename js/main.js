a4b2937b05015e335bb387ef28f7ad35bf89930d
Vu Dinh <vuvu2259@gmail.com>
2020-11-03 17:12:26 +0700

    temp commit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,36 @@
-var handleInputCalc = function (e) {
+var handleInputCalc = function (ele) {
+    // debugger;
     var input = document.getElementById('input-number')
     var perday = document.getElementById('perday')
     var perweek = document.getElementById('perweek')
     var permonth = document.getElementById('permonth')
-    var value = Number(e.value)
+    var value = Number(ele.value)
     console.log(value);
     perday.innerText = '$'+(value * 0.004).toFixed(2)
     perweek.innerText = '$'+(value * 0.004 * 7).toFixed(2)
     permonth.innerText = '$'+(value * 0.004 * 30).toFixed(2)
 }
 window.onload = function () {
-    var chooseLanguage = document.querySelector('.current-language')
+    var currentLanguage = document.querySelector('.current-language')
     handleInputCalc(document.getElementById('input-number'))
 
-    document.addEventListener('click',e=>{
-        if(Array.from(chooseLanguage.classList).indexOf('show') !== -1 && !e.path.includes(chooseLanguage)) {
-            chooseLanguage.classList.remove('show')
+    document.addEventListener('click', e => {
+        if
+        (
+            Array.from(currentLanguage.classList).indexOf('show') !== -1 &&
+            !e.path.includes(currentLanguage)
+        )
+        {
+            currentLanguage.classList.remove('show')
         }
     })
 
     var showlanguage = function () {
-        chooseLanguage.addEventListener('click', function () {
-            if(Array.from(this.classList).includes('show')){
+        currentLanguage.addEventListener('click', function () {
+            if (Array.from(this.classList).includes('show'))
                 this.classList.remove('show')
-            }else{
+            else
                 this.classList.add('show')
-            }
         })
     }
     showlanguage()
